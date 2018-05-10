848456578b1e228308f0cae66923ce41b41287f4
petter <1623144737@qq.com>
2018-05-10 21:06:20 +0800

    1st-1

--- src/components/waiting.js
+++ src/components/waiting.js
@@ -10,7 +10,7 @@ const data = [
     { name: 'test', id: 'xxxxxxxx', description: '他被广泛应用于各种电子电路实验中'},
 ]
 
-const colorlist = ['#d4237a','#1296db','#d81e06','#13227a'];
+const colorlist = ['rgb(253,255,223)','rgb(217,179,230)','rgb(208,233,255)','rgb(129,146,214)'];
 
 class Waiting extends React.Component {
     render(){
@@ -19,7 +19,7 @@ class Waiting extends React.Component {
             return(
                 <div className={style.listWrap} style={{background: colorlist[indexcolor]}} key={index}>
                     <div className={style.listLeft}>
-                        <div className={style.listItem}>设备:{item.name}</div>
+                        <div className={style.listItemTitle}>设备:{item.name}</div>
                         <div className={style.listItem}>编号:{item.id}</div>
                         <div className={style.listItem}>{item.description}</div>
                     </div>
